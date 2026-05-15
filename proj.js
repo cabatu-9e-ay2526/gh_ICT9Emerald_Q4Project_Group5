37317cb56de18374ee803f8f19c12b7256a9a3b7
cabatu-9e-ay2526 <cabatu.mh@obmontessori.edu.ph>
2026-05-15 22:42:47 +0800

    Update proj.js

--- proj.js
+++ proj.js
@@ -1,1 +1,60 @@
+function checkLogin(){
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("pass").value;
+
+
+    if(username == "admin" && password == "1234"){
+
+        alert("Login Successful!");
+
+        window.location.href = "dashboard.html";
+
+    }
+
+    else{
+
+        alert("Incorrect Username or Password");
+
+    }
+
+}
+
+function clearForm(){
+
+    document.getElementById("username").value = "";
+    document.getElementById("pass").value = "";
+
+}
+
+function submitTask(){
+
+    alert("Task Submitted!");
+
+}
+
+
+function changePassword(){
+
+    alert("Password change feature coming soon.");
+
+}
+
+
+function showQuote(){
+
+    let quotes = [
+
+        "Success starts with hard work.",
+        "Dream big and dare to fail.",
+        "Education is the key to success.",
+        "Believe in yourself."
+
+    ];
+
+    let random = Math.floor(Math.random() * quotes.length);
+
+    alert(quotes[random]);
+
+}
 
